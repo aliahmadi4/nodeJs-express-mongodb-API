e5fd5cfb34b1349b774bf0e5855670a79d4114f9
alimohammadahmadi <alimohammad.ahmadi2008@gmail.com>
2019-10-14 00:30:24 -0500

    post API / inserting new teacher to a univerity

--- routes/university.js
+++ routes/university.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const ObjectId = require('mongodb').ObjectID;
 
 router.get('/', (req, res) => {
     req.db.find().toArray((err, result) => {
@@ -7,22 +8,44 @@ router.get('/', (req, res) => {
     })
 })
 
-router.get('/:zip', (req, res) => {
-    let zip = parseInt(req.params.zip)
-    req.db.findOne({ "address.Zip": zip }, (err, result) => {
-        res.json(result);
-    });
+// router.get('/:zip', (req, res) => {
+//     let zip = parseInt(req.params.zip)
+//     req.db.findOne({ "address.Zip": zip }, (err, result) => {
+//         res.json(result);
+//     });
 
+// })
+
+router.get('/:zip', async (req, res,next) => {
+    let zip = parseInt(req.params.zip)
+    let result = await req.db.findOne({ "address.Zip": zip });
+    if(result){
+        res.json(result);
+    }
+    return next();
+})
+router.get('/:name',async(req,res)=>{
+    let result = await req.db.findOne({name:req.params.name})
+    res.json(result);
 })
 
-router.get('/nearme/:long/:lat', (req, res) => {
+router.get('/nearme/:long/:lat', async (req, res) => {
     let long = parseInt(req.params.long);
     let lat = parseInt(req.params.lat);
 
-    req.db.find({ location: { $near: [long, lat] } }).limit(2).toArray((err, doc) => {
-        res.json(doc)
+    let result = await req.db.find({ location: { $near: [long, lat] } }).limit(1);
+
+    result.toArray((err, doc) => {
+        res.json(doc);
     })
+
 })
+
+router.post("/:id/teacher", async (req,res)=>{
+    let data = await req.db.update({_id:ObjectId(req.params.id)}, {$push:{teachers:{...req.body}}});
+    res.json({successful:1});
+})
+
 
 
 module.exports = router;
